351c3e6a836f40ec79c1dd2e1afbaea9900b4d71
R3mi <kingdom.informatique@gmail.com>
2016-01-08 15:44:11 +0100

    Affichage de base de la liste des taches à faire à la date du jour

--- php/affichage.php
+++ php/affichage.php
@@ -9,4 +9,26 @@
     /*-----------------------------------*/
 ?>
 
-<center>Ici seront listées les taches à réaliser à la date d'aujourd'hui</center>
+<?php
+	global $base;
+
+	$reponse = $base->query('SELECT nomtache, datemaxtache FROM tache_2016 WHERE datetache <= CURDATE()');
+	
+	echo '<div id=\'admin\'>';
+		echo "<form name='search' action='./admin_post.php' method='POST'>";
+		echo '<table>';
+			echo '<thead><tr><th>Tache</th></tr>';
+			echo '<tbody>';
+				while ($donnees = $reponse->fetch())
+				{
+					echo '<tr>';
+						echo '<td>' . htmlspecialchars($donnees['nomtache']) . '</td>';
+					echo '</tr>';
+				}
+			echo '</tbody>';
+		echo '</table>';
+		echo '</form>';
+	echo '</div>';
+	
+	$reponse->closeCursor();
+?>
