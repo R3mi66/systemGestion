c39843589d3a43a700eab5199e0455d32e844af8
R3mi <kingdom.informatique@gmail.com>
2016-01-08 20:37:08 +0100

    Mise en forme de la liste des taches : affichage d'un ouvrage par colonne

--- php/affichage.php
+++ php/affichage.php
@@ -19,12 +19,12 @@
 	{
 		$base->query('UPDATE tache_' . $year . ' SET daterealisationtache = \'' . $now . '\' WHERE idtache=\'' . $_GET['idtache'] . '\'');
 	}
-
-	$reponse = $base->query('SELECT idtache, nomtache, datemaxtache FROM tache_' . $year . ' WHERE datetache <= CURDATE() AND daterealisationtache IS NULL ORDER BY datemaxtache');
 	
-	echo '<div id=\'admin\'>';
+	//Liste des taches pour l'Agly
+	echo '<div id=\'agly\'>';
+		$reponse = $base->query('SELECT idtache, nomtache, datemaxtache FROM tache_' . $year . ' WHERE nomtache LIKE \'%Agly%\' AND datetache <= CURDATE() AND daterealisationtache IS NULL ORDER BY datemaxtache');
 		echo '<table>';
-			echo '<thead><tr><th>Tache</th></tr>';
+			echo '<thead><tr><th>Tache Agly</th></tr>';
 			echo '<tbody>';
 				while ( $donnees = $reponse->fetch() )
 				{
@@ -42,6 +42,96 @@
 			echo '</tbody>';
 		echo '</table>';
 	echo '</div>';
-	
-	$reponse->closeCursor();
+
+	//Liste des taches pour VDR
+	echo '<div id=\'vdr\'>';
+		$reponse = $base->query('SELECT idtache, nomtache, datemaxtache FROM tache_' . $year . ' WHERE nomtache LIKE \'%VDR%\' AND datetache <= CURDATE() AND daterealisationtache IS NULL ORDER BY datemaxtache');
+		echo '<table>';
+			echo '<thead><tr><th>Tache VDR</th></tr>';
+			echo '<tbody>';
+				while ( $donnees = $reponse->fetch() )
+				{
+					echo '<tr>';
+						if ($donnees['datemaxtache'] < $now)
+						{
+							echo '<td bgcolor=\'red\'><a href=\'?idtache=' . $donnees['idtache'] . '\'>' . htmlspecialchars($donnees['nomtache']) . '</a></td>';
+						}
+						else{
+							echo '<td><a href=\'?idtache=' . $donnees['idtache'] . '\'>' . htmlspecialchars($donnees['nomtache']) . '</a></td>';
+						}
+
+					echo '</tr>';
+				}
+			echo '</tbody>';
+		echo '</table>';
+	echo '</div>';
+
+	//Liste des taches pour Vinca
+	echo '<div id=\'vinca\'>';
+		$reponse = $base->query('SELECT idtache, nomtache, datemaxtache FROM tache_' . $year . ' WHERE nomtache LIKE \'%Vinca%\' AND datetache <= CURDATE() AND daterealisationtache IS NULL ORDER BY datemaxtache');
+		echo '<table>';
+			echo '<thead><tr><th>Tache Vinca</th></tr>';
+			echo '<tbody>';
+				while ( $donnees = $reponse->fetch() )
+				{
+					echo '<tr>';
+						if ($donnees['datemaxtache'] < $now)
+						{
+							echo '<td bgcolor=\'red\'><a href=\'?idtache=' . $donnees['idtache'] . '\'>' . htmlspecialchars($donnees['nomtache']) . '</a></td>';
+						}
+						else{
+							echo '<td><a href=\'?idtache=' . $donnees['idtache'] . '\'>' . htmlspecialchars($donnees['nomtache']) . '</a></td>';
+						}
+
+					echo '</tr>';
+				}
+			echo '</tbody>';
+		echo '</table>';
+	echo '</div>';
+
+	//Liste des taches pour le Reseau
+	echo '<div id=\'reseau\'>';
+		$reponse = $base->query('SELECT idtache, nomtache, datemaxtache FROM tache_' . $year . ' WHERE nomtache LIKE \'%Reseau%\' AND datetache <= CURDATE() AND daterealisationtache IS NULL ORDER BY datemaxtache');
+		echo '<table>';
+			echo '<thead><tr><th>Tache Reseau</th></tr>';
+			echo '<tbody>';
+				while ( $donnees = $reponse->fetch() )
+				{
+					echo '<tr>';
+						if ($donnees['datemaxtache'] < $now)
+						{
+							echo '<td bgcolor=\'red\'><a href=\'?idtache=' . $donnees['idtache'] . '\'>' . htmlspecialchars($donnees['nomtache']) . '</a></td>';
+						}
+						else{
+							echo '<td><a href=\'?idtache=' . $donnees['idtache'] . '\'>' . htmlspecialchars($donnees['nomtache']) . '</a></td>';
+						}
+
+					echo '</tr>';
+				}
+			echo '</tbody>';
+		echo '</table>';
+	echo '</div>';
+
+	//Liste des taches pour les stations
+	echo '<div id=\'station\'>';
+		$reponse = $base->query('SELECT idtache, nomtache, datemaxtache FROM tache_' . $year . ' WHERE nomtache LIKE \'%Station%\' AND datetache <= CURDATE() AND daterealisationtache IS NULL ORDER BY datemaxtache');
+		echo '<table>';
+			echo '<thead><tr><th>Tache Station</th></tr>';
+			echo '<tbody>';
+				while ( $donnees = $reponse->fetch() )
+				{
+					echo '<tr>';
+						if ($donnees['datemaxtache'] < $now)
+						{
+							echo '<td bgcolor=\'red\'><a href=\'?idtache=' . $donnees['idtache'] . '\'>' . htmlspecialchars($donnees['nomtache']) . '</a></td>';
+						}
+						else{
+							echo '<td><a href=\'?idtache=' . $donnees['idtache'] . '\'>' . htmlspecialchars($donnees['nomtache']) . '</a></td>';
+						}
+
+					echo '</tr>';
+				}
+			echo '</tbody>';
+		echo '</table>';
+	echo '</div>';
 ?>
