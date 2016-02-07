88c35abc2a112b7fc8c7c9d30eceb6f54afa78a4
R3mi <kingdom.informatique@gmail.com>
2016-02-07 14:10:50 +0100

    Mise en place d'onglet pour les pages 1 et 2

--- php/navigation.php
+++ php/navigation.php
@@ -9,7 +9,7 @@
     /*-----------------------------------*/
 ?>
 
-<ul>
-	<li><a class="btn" href="?page=1">Taches en cours</a></li>
-	<li><a class="btn" href="?page=2">Liste complete</a></li>
+<ul class="nav nav-tabs">
+	<li <?php if((!isset($_GET["page"]) OR ($_GET["page"]==1))){?>class="active"<?php } ?>><a href="?page=1">Taches en cours</a></li>
+	<li <?php if((isset($_GET["page"]) AND ($_GET["page"]==2))){?>class="active"<?php } ?>><a href="?page=2">Liste complete</a></li>
 </ul>
